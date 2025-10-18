ec7ea2c875d8c4db20ecc36eafc32b5f2ee49309
Wyatt Roersma <wyattroersma@gmail.com>
2025-10-18 15:51:40 -0400

    Initial Commit

---
+++ StatFlux/StatFluxApp.swift
@@ -0,0 +1,32 @@
+//
+//  StatFluxApp.swift
+//  StatFlux
+//
+//  Created by Wyatt Roersma on 10/18/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StatFluxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ StatFluxTests/StatFluxTests.swift
@@ -0,0 +1,16 @@
+//
+//  StatFluxTests.swift
+//  StatFluxTests
+//
+//  Created by Wyatt Roersma on 10/18/25.
+//
+
+import Testing
+
+struct StatFluxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
